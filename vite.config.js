24dcb1b2522d32902e8ed6e721feb8d474df6f76
ccmyk <31525201+ccmyk@users.noreply.github.com>
2025-10-19 01:28:00 -0700

    chore: scaffold tooling

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+    open: false,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: { main: './index.html' },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
+  plugins: [
+    glsl({
+      include: /\.(glsl|vs|fs)$/i,
+      warnDuplicatedImports: false,
+      compress: false,
+    }),
+  ],
+});
